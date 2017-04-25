203459d68c9dc5e9edf1e314eb21cba46d0ea7f5
DiddyManilla <diddymanilla@gmail.com>
2017-04-25 10:37:23 -0600

    Question appears with answers when player comes close to NPC.

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 $(function() {
 	var images = ['Partido'];
 	var keys = {left: false, right: false};
+	var questions = [{
+		'text': 'Buenos dias! Adonde vas?',
+		'answers' : ['Buenos dias! Yo voy al Partido.',
+					 'Buenos dias! Yo voy a la Partido',
+					 'Buenos dias! Yo vas a el Partido.',
+					 'Buenos dias! Yo vamos al Partido.']
+	},
+	{
+		'text': 'Hola! Como estas?',
+		'answers': [
+
+					]
+	}];
 	var player = $('#player');
 	player.speed = 5;
+	
+	player.score = 0;
+	player.maxScore = 2;
 
 	player.css('height', $('#player-icon').css('height'));
 	player.css('width', $('#player-icon').css('width'));
@@ -20,27 +36,51 @@ $(function() {
 		}
 	}
 
+	function moveLeft() {
+		player.css('left', player.position().left - 1 + 'px');
+		player.timeout = setTimeout(arguments.callee, player.speed);
+	}
+	function moveRight() {
+		player.css('left', player.position().left + 1 + 'px');
+		player.timeout = setTimeout(arguments.callee, player.speed);
+
+		if (player.position().left + player.width() >= $('#background').width()) {
+			nextScreen();
+			player.css('left', '0px');
+		} else if (player.position().left + player.width() >= $('#person').position().left - 50) {
+			clearTimeout(player.timeout);
+			question();
+		}
+	}
+
+	function answer() {
+
+	}
+	function question() {
+		if (questions.length) {
+			var question = questions.shift();
+			$('#question').text(question.text);
+			$('#answers li').each(function(i) {
+				$(this).text(question.answers[i]);
+			});
+
+			$('#answers li').on('click', function(event) {
+				answer()
+			});
+
+			$('#question-box').removeClass('hidden');
+			$('#question-box').addClass('show');
+		}
+	}
+
 	$('body').on('keydown', function(event) {
 
 		if (event.keyCode == 37 && !keys.left) {
 			keys.left = true;
-			(function() {
-				player.css('left', player.position().left - 1 + 'px');
-				player.timeout = setTimeout(arguments.callee, player.speed);
-			})();
-
+			moveLeft();
 		} else if (event.keyCode == 39 && !keys.right) {
-
 			keys.right = true;
-			(function() {
-				player.css('left', player.position().left + 1 + 'px');
-				if (player.position().left + player.width() >= $('#background').width()) {
-					nextScreen();
-					player.css('left', '0px');
-				}
-				player.timeout = setTimeout(arguments.callee, player.speed);
-			})();
-
+			moveRight();
 		}
 	})
 
